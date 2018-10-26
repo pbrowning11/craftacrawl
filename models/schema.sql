334b88b83893805395827a964a16ebf6a250f434
Breezy <pbrowning11@gmail.com>
2018-10-26 13:54:31 -0400

    csv import into table sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS CraftACrawldb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE CraftACrawldb;
+
+USE CraftACrawldb;
+
+CREATE TABLE crawls (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    street VARCHAR(45) NULL,
+    zip INT(5) NULL,
+    cat1 VARCHAR(30) NULL,
+    cat2 VARCHAR(30) NULL,
+    website VARCHAR(100) NULL,
+    phone INT(10) NULL,
+    PRIMARY KEY (id)
+);
