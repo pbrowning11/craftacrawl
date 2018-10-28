fac6852432c6a676eb4930c08c881711d8223681
Killer Coder <Maxwell.w.allee@gmail.com>
2018-10-28 18:21:17 -0400

    a bunch of mysql stuff

---
+++ models/bar.js
@@ -0,0 +1,44 @@
+module.exports = function(sequelize, DataTypes) {
+  var Bar = sequelize.define("Bar", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    neighborhood: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    cat1: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    cat2: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    checked: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }
+  })
+  return Bar
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,40 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  app.post("/api/posts", function (req, res) {
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+    console.log(req.body.barList);
+    db.Crawl.create({
+      crawlName: req.body.crawlName,
+      barList: req.body.barList.toString()
+    })
+      .then(function (dbcrawl) {
+        res.json(dbcrawl);
+      });
   });
+  app.get("/crawl/:crawl", function (req, res) {
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Crawl.findOne({
+      where: {
+        crawlName: req.params.crawl
+      }
+    }).then(function (crawlInfo) {
+      var barArray = []
+      var crawlArray = JSON.parse("[" + crawlInfo.dataValues.barList + "]")
+      crawlArray.forEach(function (position) {
+        db.Bar.findOne({
+          where: {
+            id: position
+          }
+        }).then(function (barInfo) {
+          console.log(barInfo.dataValues)
+          barArray.push(barInfo.dataValues)
+        });
+      });
+
+      console.log(barArray);
+      res.json(barArray);
     });
   });
-};
+}
