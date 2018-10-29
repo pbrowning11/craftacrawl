f3aa40e86554c8e760a354136e4e79821910eae1
Joxel <joxelstudios@gmail.com>
2018-10-29 13:38:33 -0400

    added google api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,10 +5,29 @@ module.exports = function (app) {
   app.post("/api/posts", function (req, res) {
 
     console.log(req.body.barList);
+    // Google Maps API Query Start    
+    var apikey = "AIzaSyDu0Qtc37kImb-6q2CGWi-T9DeM0s80ZIk&"
+    var params = {
+      origin: "" + "Charlotte+NC&", // Starting Address    
+      mode: "walking&", // Mode of Travel; Can be 'driving', 'walking', 'bicycling', or 'transit';
+      waypoints: "waypoints=optimize:true",
+      // Waypoints are extra stops in between the origin and destination; To be formatted as follows:
+      // &waypoints=Charlotte|Raleigh|... 
+      // You can supply one or more locations separated by the pipe character (|), in the form of an address, latitude/longitude coordinates, or a place ID:
+    }
+    var queryurl = "https://maps.googleapis.com/maps/api/directions/json?" + params + apikey;
+
+
+
+
+
+
+
+
     db.Crawl.create({
-      crawlName: req.body.crawlName,
-      barList: req.body.barList.toString()
-    })
+        crawlName: req.body.crawlName,
+        barList: req.body.barList.toString()
+      })
       .then(function (dbcrawl) {
         res.json(dbcrawl);
       });
