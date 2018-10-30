3ef13403bda5205ab7f1e3c0766d0f86b8afe608
Joxel <joxelstudios@gmail.com>
2018-10-30 13:00:21 -0400

    Finished Google API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,51 +1,84 @@
 var db = require("../models");
+var request = require('request');
 
 module.exports = function (app) {
-  // Get all examples
+
   app.post("/api/googleapi", function (req, res) {
     var barArr = JSON.parse("[" + req.body.barList + "]");
     var barArr2 = [];
-    console.log('');    
+    console.log('');
     console.log("These are the ids of the filtered bars from the database: " + barArr);
-    var morePromises=[]
+    var morePromises = [];
+    var correctedArr = [];
 
     barArr.forEach(function (bar) {
       morePromises.push(
-      db.Bar.findOne({
-        where: {
-          id: bar
-        }
-      }))
-      Promise.all(morePromises).then(function (res) {
-        // console.log(res.street + ", " + res.zip);
-        barArr2.push(res.street + "," + res.zip);
-        console.log('');
-        console.log("Bar address array:");
-        console.log(barArr2);
-        
-      });
-
+        db.Bar.findOne({
+          where: {
+            id: bar
+          }
+        }))
     });
-    // Google Maps API Query Start    
-    var apikey = "AIzaSyDu0Qtc37kImb-6q2CGWi-T9DeM0s80ZIk&"
+    Promise.all(morePromises).then(function (res) {
+      res.forEach(element => {
+        barArr2.push(element.street + "," + element.zip)
+      });
+      console.log("Bar address array:");
+      console.log(barArr2);
 
-    // Wanna learn how to use parameters as an object...
-    // var params = {
-    //   origin: barArr2[0] + "Charlotte+NC&", // Starting Address 
-    //   destination: barArr2[1],   
-    //   mode: "walking&", // Mode of Travel; Can be 'driving', 'walking', 'bicycling', or 'transit';
-    //   waypoints: "waypoints=optimize:true",
-    //   // Waypoints are extra stops in between the origin and destination; To be formatted as follows:
-    //   // &waypoints=Charlotte|Raleigh|... 
-    //   // You can supply one or more locations separated by the pipe character (|), in the form of an address, latitude/longitude coordinates, or a place ID:
-    // }
+      // Google Maps API Query Start    
+      var apikey = "&key=AIzaSyDu0Qtc37kImb-6q2CGWi-T9DeM0s80ZIk&"
 
-    var queryUrl = "https://maps.googleapis.com/maps/api/directions/json?origin="+ barArr2[0] + "&destination=" + barArr2[1] + "&mode=walking&waypoints=optimize:true|"
-    for (var i = 2; i > barArr2.length; i++){
-      queryUrl += (barArr2[i]+"|");
-      // console.log(queryUrl);
-    }
-    
+      // Wanna learn how to use parameters as an object...
+      // var params = {
+      //   origin: barArr2[0] + "Charlotte+NC&", // Starting Address 
+      //   destination: barArr2[1],   
+      //   mode: "walking&", // Mode of Travel; Can be 'driving', 'walking', 'bicycling', or 'transit';
+      //   waypoints: "waypoints=optimize:true",
+      //   // Waypoints are extra stops in between the origin and destination; To be formatted as follows:
+      //   // &waypoints=Charlotte|Raleigh|... 
+      //   // You can supply one or more locations separated by the pipe character (|), in the form of an address, latitude/longitude coordinates, or a place ID:
+      // }
+
+      var queryUrl = "https://maps.googleapis.com/maps/api/directions/json?origin=" + barArr2[0] + "&destination=" + barArr2[1] + "&mode=walking&waypoints=optimize:true|";
+      console.log('------------------------------------');
+      console.log("Running URL address loop for " + Number(barArr2.length - 2) + " addresses!");
+      console.log('------------------------------------');
+
+      for (var i = 2; i < barArr2.length; i++) {
+        queryUrl = queryUrl + (barArr2[i] + "|");
+      };
+      queryUrl = queryUrl + apikey;
+      console.log("");
+      console.log(queryUrl);
+
+      request(queryUrl, function (error, response, body) {
+        if (error) {
+          console.log("error:", error);
+        }; // Print the error if one occurred        
+        console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+        console.log("Google API:");
+        var google = JSON.parse(body).routes[0];
+        // console.log(JSON.parse(body).routes[0].legs);
+        console.log("Starting Address: " + google.legs[0].start_address);
+        console.log("Waypoint order: " + google.waypoint_order);
+        google.legs.forEach(element => {
+          console.log(element.end_address);
+        });
+        console.log(google.waypoint_order);
+        console.log("");
+        console.log("Bar IDs in original order:" );
+        console.log(barArr);
+        correctedArr.push(barArr[0]);
+        for (var i = 0; i < google.waypoint_order.length; i++){
+          var waypointId = google.waypoint_order[i] + 2;
+          correctedArr.push(barArr[waypointId]);
+        }
+        correctedArr.push(barArr[1]);
+        console.log ("Adjusted Route:");
+        console.log (correctedArr);
+      });
+    });
   });
 
   app.post("/api/posts", function (req, res) {
@@ -62,8 +95,9 @@ module.exports = function (app) {
     db.Crawl.create(req.body).then(function (dbcrawl) {
       res.json(dbcrawl);
     });
-
   });
+
+
   app.get("/crawl/:crawl", function (req, res) {
     db.Crawl.findOne({
       where: {
