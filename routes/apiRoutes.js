d571b735928c127fd8fac27b231ad12f90335c09
Max Allee <maxwell.w.allee@gmail.com>
2019-12-07 17:44:23 -0500

    added ability to strike through a bar

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,6 +6,11 @@ require('dotenv').config()
 module.exports = function (app) {
 
   app.post("/api/googleapi/", function (req, res) {
+    if (!req.user) {
+      console.log('not logged')
+      res.sendStatus(404)
+      return
+    }
     var barArr = JSON.parse("[" + req.body.barList + "]");
     var groupNam = req.body.crawlName
     var barArr2 = [];
@@ -61,19 +66,14 @@ module.exports = function (app) {
           console.log("error:", error);
         }
         ; // Print the error if one occurred
-        console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-        console.log("Google API:");
+
         var google = JSON.parse(body).routes[0];
         // console.log(JSON.parse(body).routes[0].legs);
-        console.log("Starting Address: " + google.legs[0].start_address);
-        console.log("Waypoint order: " + google.waypoint_order);
+
         google.legs.forEach(element => {
-          console.log(element.end_address);
+
         });
-        console.log(google.waypoint_order);
-        console.log("");
-        console.log("Bar IDs in original order:");
-        console.log(barArr);
+
         correctedArr.push(barArr[0]);
         for (var i = 0; i < google.waypoint_order.length; i++) {
           var waypointId = google.waypoint_order[i] + 2;
@@ -83,12 +83,13 @@ module.exports = function (app) {
         console.log("Adjusted Route:");
         console.log(correctedArr);
         // pushIt(correctedArr,groupNam)
-        console.log("I made it all the way here")
+        console.log("I made it all the way here", req.user.id)
 
 
         db.Crawl.create({
+          owner: req.user.id,
           crawlName: groupNam,
-          barList: correctedArr.toString()
+          barList: correctedArr.toString(),
         })
           .then(function (dbcrawl) {
             console.log(dbcrawl)
@@ -132,20 +133,23 @@ module.exports = function (app) {
         promises.push(
           db.Bar.findOne({
             where: {
-              id: position
+              id: position,
+              
             }
           }));
       });
       Promise.all(promises).then(function (barsAr) {
         console.log(barArray);
         res.render("results", {
-          bars: barsAr
+          bars: barsAr,
+          whereAt : crawlInfo.dataValues.position
+            
         });
       });
     });
   });
 
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     res.json("/")
   });
 
@@ -185,38 +189,77 @@ module.exports = function (app) {
       });
     });
   });
-  app.get("/api/category/:cat", function(req, res) {
+  app.get("/api/category/:cat", function (req, res) {
     console.log(req.params.hood);
     db.Bar.findAll({
       where: {
         cat1: req.params.cat
       }
-    }).then(function(sendthehood) {
+    }).then(function (sendthehood) {
       res.render("hood", {
         hood: sendthehood
       });
     });
   });
 
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     req.session.destroy();
     res.redirect("/")
   });
 
-  app.post("/submit", function(req, res) {
-    db.submittedBar.create(
-      req.body
-    ).then(function(newBar) {
-      res.json(newBar);
-    })
-  })
-  app.get("/api/loginCheck",(req, res)=>{
-    console.log("checking")
-    if(req.user){
-      res.send({login: true})
-    }else{
-      res.send({login: false})
+  app.post("/submit", function (req, res) {
+    if (req.user) {
+      console.log('==============================================================================================')
+      console.log(req.body)
+
+      console.log('==============================================================================================')
+      db.submittedBar.create(
+        req.body
+      ).then(function (newBar) {
+        res.json(newBar);
+      })
+    } else {
+      res.sendStatus(404)
     }
   })
+  app.get("/api/loginCheck", (req, res) => {
+    console.log("checking")
+    if (req.user) {
+      console.log(req.user)
+      res.send({ login: true })
+    } else {
+      res.send({ login: false })
+    }
+  })
+  app.get("/api/isOwner/:crawl", (req, res) => {
+
+    db.Crawl.findOne({
+      where: {
+        crawlName: req.params.crawl
+      }
+    }).then(function (crawlInfo) {
+      if (req.user) {
+        console.log(crawlInfo.owner, req.user.id, '<=====')
+        console.log(crawlInfo.owner === req.user.id)
+        if (crawlInfo.owner === req.user.id) {
+          res.json({ rightUser: true })
+          return
+        }
+        res.json({ rightUser: false })
+      }
+      res.json({ rightUser: false })
+    })
+  })
+  app.post("/api/updateLocation/", (req, res) => {
+    console.log(req.body.crawld)
+    db.Crawl.update({
+      position: parseInt(req.body.position)+1
+    },{ where: { crawlName: req.body.crawld, owner: req.user.id } })
+     .then((resultz)=>{
+       console.log(resultz)
+       res.sendStatus(200)
+     })
+  })
+
 };
